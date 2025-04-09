c335881814ac5f376e862046f9935ade61b62a5e
bradyshehu <shehu.brady@gmail.com>
2025-04-09 15:58:48 +0200

    light switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-const offLampEl = document.querySelector(".off-lamp");
+const offLampEl = document.querySelector("#off-lamp");
 console.log(offLampEl);
+const onLampEl = document.querySelector("#on-lamp");
+console.log(onLampEl);
+const switchEl = document.querySelector("#switch");
+console.log(switchEl);
+
+switchEl.addEventListener("click", function () {
+  if (offLampEl.classList.contains("d-block")) {
+    offLampEl.classList.replace("d-block", "d-none");
+    onLampEl.classList.replace("d-none", "d-block");
+  } else {
+    offLampEl.classList.replace("d-none", "d-block");
+    onLampEl.classList.replace("d-block", "d-none");
+  }
+});
